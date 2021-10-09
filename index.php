4fb7e23b4a753e7ca18740c006955c0864b6154b
Anis KCHAOU <kchaouanis26@gmail.com>
2021-10-09 19:51:55 +0200

    add php file

---
+++ index.php
@@ -0,0 +1,444 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>
+      CoderDocs - Bootstrap 5 Documentation Template For Software Projects
+    </title>
+
+    <!-- Meta -->
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta
+      name="description"
+      content="Bootstrap Documentation Template For Software Developers"
+    />
+    <meta name="author" content="Xiaoying Riley at 3rd Wave Media" />
+    <link rel="shortcut icon" href="favicon.ico" />
+
+    <!-- Google Font -->
+    <link
+      href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700&display=swap"
+      rel="stylesheet"
+    />
+
+    <!-- FontAwesome JS-->
+    <script defer src="assets/fontawesome/js/all.min.js"></script>
+
+    <!-- Theme CSS -->
+    <link id="theme-style" rel="stylesheet" href="assets/css/theme.css" />
+  </head>
+
+  <body>
+    <header class="header fixed-top">
+      <div class="branding docs-branding">
+        <div class="container-fluid position-relative py-2">
+          <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <img
+              style="display: block; margin: auto"
+              src="assets/logos/logo.png"
+            />
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarNavDropdown"
+              aria-controls="navbarNavDropdown"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+              <ul class="navbar-nav">
+                <li class="nav-item dropdown">
+                  <a
+                    class="nav-link dropdown-toggle"
+                    href="#"
+                    id="navbarDropdownMenuLink"
+                    data-toggle="dropdown"
+                    aria-haspopup="true"
+                    aria-expanded="false"
+                  >
+                    HR
+                  </a>
+                  <div
+                    class="dropdown-menu"
+                    aria-labelledby="navbarDropdownMenuLink"
+                  >
+                    <a class="dropdown-item" href="#">Humant</a>
+                  </div>
+                </li>
+
+                <li class="nav-item dropdown">
+                  <a
+                    class="nav-link dropdown-toggle"
+                    href="#"
+                    id="navbarDropdownMenuLink"
+                    data-toggle="dropdown"
+                    aria-haspopup="true"
+                    aria-expanded="false"
+                  >
+                    Medical
+                  </a>
+                  <div
+                    class="dropdown-menu"
+                    aria-labelledby="navbarDropdownMenuLink"
+                  >
+                    <a class="dropdown-item" href="#">LifeCare</a>
+                    <a class="dropdown-item" href="#">PharmaLife</a>
+                    <a class="dropdown-item" href="#">Dentic</a>
+                    <a class="dropdown-item" href="#">SmartLab</a>
+                  </div>
+                </li>
+
+                <li class="nav-item dropdown">
+                  <a
+                    class="nav-link dropdown-toggle"
+                    href="#"
+                    id="navbarDropdownMenuLink"
+                    data-toggle="dropdown"
+                    aria-haspopup="true"
+                    aria-expanded="false"
+                  >
+                    Services
+                  </a>
+                  <div
+                    class="dropdown-menu"
+                    aria-labelledby="navbarDropdownMenuLink"
+                  >
+                    <a class="dropdown-item" href="carrent.html">CarRental</a>
+                    <a class="dropdown-item" href="#">Computer project</a>
+                    <a class="dropdown-item" href="#">GYM</a>
+                  </div>
+                </li>
+
+                <li class="nav-item dropdown">
+                  <a
+                    class="nav-link dropdown-toggle"
+                    href="#"
+                    id="navbarDropdownMenuLink"
+                    data-toggle="dropdown"
+                    aria-haspopup="true"
+                    aria-expanded="false"
+                  >
+                    Industry
+                  </a>
+                  <div
+                    class="dropdown-menu"
+                    aria-labelledby="navbarDropdownMenuLink"
+                  >
+                    <a class="dropdown-item" href="megasteel.html">MegaSteel</a>
+                    <a class="dropdown-item" href="#">Managem</a>
+                    <a class="dropdown-item" href="#">ERP Base</a>
+                    <a class="dropdown-item" href="stockbay.html">Stockbay</a>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+        <!--//container-->
+      </div>
+      <!--//branding-->
+    </header>
+    <!--//header-->
+
+    <div class="page-header theme-bg-dark py-5 text-center position-relative">
+      <div class="theme-bg-shapes-right"></div>
+      <div class="theme-bg-shapes-left"></div>
+      <div class="container">
+        <h1 class="page-heading single-col-max mx-auto">Documentation</h1>
+        <div class="page-intro single-col-max mx-auto">
+          Everything you need to get your software documentation online.
+        </div>
+        <div class="main-search-box pt-3 d-block mx-auto">
+          <form class="search-form w-100">
+            <input
+              type="text"
+              placeholder="Search the docs..."
+              name="search"
+              class="form-control search-input"
+            />
+            <button type="submit" class="btn search-btn" value="Search">
+              <i class="fas fa-search"></i>
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+    <!--//page-header-->
+    <div class="page-content">
+      <div class="container">
+        <div class="docs-overview py-5">
+          <div class="row justify-content-center">
+            <div class="col-12 col-lg-4 py-3">
+              <div class="card shadow-sm">
+                <div class="card-body">
+                  <h5 class="card-title mb-3">
+                    <span class="card-title-text">Megasteel</span>
+                  </h5>
+                  <div class="card-text">
+                    <img
+                      style="display: block; margin: auto"
+                      src="assets/logos/megasteel.png"
+                    />
+                  </div>
+                  <a class="card-link-mask" href="megasteel.html"></a>
+                </div>
+                <!--//card-body-->
+              </div>
+              <!--//card-->
+            </div>
+            <!--//col-->
+            <div class="col-12 col-lg-4 py-3">
+              <div class="card shadow-sm">
+                <div class="card-body">
+                  <h5 class="card-title mb-3">
+                    <span class="card-title-text">stockbay</span>
+                  </h5>
+                  <div class="card-text">
+                    <img
+                      style="display: block; margin: auto"
+                      src="assets/logos/stockbay.png"
+                    />
+                  </div>
+                  <a class="card-link-mask" href="stockbay.html"></a>
+                </div>
+                <!--//card-body-->
+              </div>
+              <!--//card-->
+            </div>
+            <!--//col-->
+            <div class="col-12 col-lg-4 py-3">
+              <div class="card shadow-sm">
+                <div class="card-body">
+                  <h5 class="card-title mb-3">
+                    <span class="card-title-text">Rentalcar</span>
+                  </h5>
+                  <div class="card-text">
+                    <img
+                      style="display: block; margin: auto"
+                      src="assets/logos/carrental.png"
+                    />
+                  </div>
+                  <a class="card-link-mask" href="carrent.html"></a>
+                </div>
+                <!--//card-body-->
+              </div>
+              <!--//card-->
+            </div>
+            <!--//col-->
+            <div class="col-12 col-lg-4 py-3">
+              <div class="card shadow-sm">
+                <div class="card-body">
+                  <h5 class="card-title mb-3">
+                    <span class="card-title-text">Lifecare</span>
+                  </h5>
+                  <div class="card-text">
+                    <img
+                      style="display: block; margin: auto"
+                      src="assets/logos/lifecare.png"
+                    />
+                  </div>
+                  <a class="card-link-mask" href="#"></a>
+                </div>
+                <!--//card-body-->
+              </div>
+              <!--//card-->
+            </div>
+            <!--//col-->
+            <div class="col-12 col-lg-4 py-3">
+              <div class="card shadow-sm">
+                <div class="card-body">
+                  <h5 class="card-title mb-3">
+                    <span class="card-title-text">Accountt</span>
+                  </h5>
+                  <div class="card-text">
+                    <img
+                      style="display: block; margin: auto"
+                      src="assets/logos/accountt.png"
+                    />
+                  </div>
+                  <a class="card-link-mask" href="#"></a>
+                </div>
+                <!--//card-body-->
+              </div>
+              <!--//card-->
+            </div>
+            <!--//col-->
+            <div class="col-12 col-lg-4 py-3">
+              <div class="card shadow-sm">
+                <div class="card-body">
+                  <h5 class="card-title mb-3">
+                    <span class="card-title-text">Librarylab</span>
+                  </h5>
+                  <div class="card-text">
+                    <img
+                      style="display: block; margin: auto"
+                      src="assets/logos/librarylab.png"
+                    />
+                  </div>
+                  <a class="card-link-mask" href="#"></a>
+                </div>
+                <!--//card-body-->
+              </div>
+              <!--//card-->
+            </div>
+            <!--//col-->
+            <div class="col-12 col-lg-4 py-3">
+              <div class="card shadow-sm">
+                <div class="card-body">
+                  <h5 class="card-title mb-3">
+                    <span class="card-title-text">ERPBase</span>
+                  </h5>
+                  <div class="card-text">
+                    <img
+                      style="display: block; margin: auto"
+                      src="assets/logos/erpbase.png"
+                    />
+                  </div>
+                  <a class="card-link-mask" href="#"></a>
+                </div>
+                <!--//card-body-->
+              </div>
+              <!--//card-->
+            </div>
+            <!--//col-->
+            <div class="col-12 col-lg-4 py-3">
+              <div class="card shadow-sm">
+                <div class="card-body">
+                  <h5 class="card-title mb-3">
+                    <span class="card-title-text">Humant</span>
+                  </h5>
+                  <div class="card-text">
+                    <img
+                      style="display: block; margin: auto"
+                      src="assets/logos/humant.png"
+                    />
+                  </div>
+                  <a class="card-link-mask" href="#"></a>
+                </div>
+                <!--//card-body-->
+              </div>
+              <!--//card-->
+            </div>
+            <!--//col-->
+            <div class="col-12 col-lg-4 py-3">
+              <div class="card shadow-sm">
+                <div class="card-body">
+                  <h5 class="card-title mb-3">
+                    <span class="card-title-text">Dentic</span>
+                  </h5>
+                  <div class="card-text">
+                    <img
+                      style="display: block; margin: auto"
+                      src="assets/logos/dentic.png"
+                    />
+                  </div>
+                  <a class="card-link-mask" href="#"></a>
+                </div>
+                <!--//card-body-->
+              </div>
+              <!--//card-->
+            </div>
+            <!--//col-->
+          </div>
+          <!--//row-->
+        </div>
+        <!--//container-->
+      </div>
+      <!--//container-->
+    </div>
+    <!--//page-content-->
+    <!--//cta-section
+    <section
+      class="cta-section text-center py-5 theme-bg-dark position-relative"
+    >
+      <div class="theme-bg-shapes-right"></div>
+      <div class="theme-bg-shapes-left"></div>
+      <div class="container">
+        <h3 class="mb-2 text-white mb-3">
+          Launch Your Software Project Like A Pro
+        </h3>
+        <div class="section-intro text-white mb-3 single-col-max mx-auto">
+          Want to launch your software project and start getting traction from
+          your target users? Check out our premium
+          <a
+            class="text-white"
+            href="https://themes.3rdwavemedia.com/bootstrap-templates/startup/coderpro-bootstrap-5-startup-template-for-software-projects/"
+            >Bootstrap 5 startup template CoderPro</a
+          >! It has everything you need to promote your product.
+        </div>
+        <div class="pt-3 text-center">
+          <a
+            class="btn btn-light"
+            href="https://themes.3rdwavemedia.com/bootstrap-templates/startup/coderpro-bootstrap-5-startup-template-for-software-projects/"
+            >Get CoderPro<i class="fas fa-arrow-alt-circle-right ml-2"></i
+          ></a>
+        </div>
+      </div>
+    </section>
+    -->
+
+    <footer class="footer">
+      <div class="footer-bottom text-center py-5">
+        <ul class="social-list list-unstyled pb-4 mb-0">
+          <li class="list-inline-item">
+            <a href="#"><i class="fab fa-youtube fa-fw"></i></a>
+          </li>
+          <li class="list-inline-item">
+            <a href="#"><i class="fab fa-twitter fa-fw"></i></a>
+          </li>
+          <li class="list-inline-item">
+            <a href="#"><i class="fab fa-slack fa-fw"></i></a>
+          </li>
+          <li class="list-inline-item">
+            <a href="#"><i class="fab fa-product-hunt fa-fw"></i></a>
+          </li>
+        </ul>
+        <!--//social-list-->
+
+        <!--/* This template is free as long as you keep the footer attribution link. If you'd like to use the template without the attribution link, you can buy the commercial license via our website: themes.3rdwavemedia.com Thank you for your support. :) */-->
+        <small class="copyright"
+          >Designed with <i class="fas fa-heart" style="color: #fb866a"></i> by
+          <a
+            class="theme-link"
+            href="http://themes.3rdwavemedia.com"
+            target="_blank"
+            >Xiaoying Riley</a
+          >
+          for developers</small
+        >
+      </div>
+    </footer>
+
+    <!-- Javascript -->
+    <script src="assets/plugins/popper.min.js"></script>
+    <script src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Page Specific JS -->
+    <script src="assets/plugins/smoothscroll.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.8/highlight.min.js"></script>
+    <script src="assets/js/highlight-custom.js"></script>
+    <script src="assets/plugins/simplelightbox/simple-lightbox.min.js"></script>
+    <script src="assets/plugins/gumshoe/gumshoe.polyfills.min.js"></script>
+    <script src="assets/js/docs.js"></script>
+
+    <script
+      src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+      integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+      integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+      integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
